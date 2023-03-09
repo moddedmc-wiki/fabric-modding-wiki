f0a9866868a4cd17325f92107873d714c66f8178
Calum <93472213+mineblock11@users.noreply.github.com>
2023-03-09 23:16:52 +0000

    Open Source Final Stuff (#26)

--- doks-theme/assets/js/scripts.js
+++ doks-theme/assets/js/scripts.js
@@ -171,3 +171,29 @@
 		$( 'body' ).toggleClass( 'offcanvas-expanded' );
 	} );
 }( jQuery ) );
+
+// assets/js/post.js
+var codeBlocks = document.querySelectorAll('pre.highlight');
+
+codeBlocks.forEach(function (codeBlock) {
+  var copyButton = document.createElement('button');
+  copyButton.className = 'copy btn--dark btn--rounded';
+  copyButton.type = 'button';
+  copyButton.ariaLabel = 'Copy code to clipboard';
+  copyButton.innerText = 'Copy';
+
+  codeBlock.append(copyButton);
+
+  copyButton.addEventListener('click', function () {
+    var code = codeBlock.querySelector('code').innerText.trim();
+    window.navigator.clipboard.writeText(code);
+
+    copyButton.innerText = 'Copied';
+
+    setTimeout(function () {
+      copyButton.innerText = 'Copy';
+	  copyButton.blur()
+    }, 750);
+
+  });
+});
