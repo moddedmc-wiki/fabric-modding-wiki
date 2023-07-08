e7e11fb7195cc04b7158d49ad0f085f953878852
Calum <contact@mineblock11.dev>
2023-07-08 17:17:51 +0100

    Cleanup using studio

--- tokens.config.ts
+++ tokens.config.ts
@@ -27,5 +27,18 @@ export default defineTheme({
       800: "#D84315",
       900: "#BF360C"
     }
+  },
+
+  font: {
+    sans: "apple-system,BlinkMacSystemFont,\"Segoe UI\",Roboto,Helvetica,Arial,sans-serif,\"Apple Color Emoji\",\"Segoe UI Emoji\",\"Segoe UI Symbol\"",
+    mono: "monospace"
+  },
+
+  docus: {
+    loadingBar: {
+      gradientColorStop1: "#dc2c00",
+      gradientColorStop2: "#fec134",
+      gradientColorStop3: "#a9e100"
+    }
   }
 })
