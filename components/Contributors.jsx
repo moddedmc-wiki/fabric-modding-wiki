6e058a68b94fd70ba5fb5aab5b764b08e5f289e5
Calum <contact@mineblock11.dev>
2023-07-21 21:07:42 +0100

    Contributors list

---
+++ components/Contributors.jsx
@@ -0,0 +1,25 @@
+import { Cards, Card } from "nextra/components";
+
+export function Contributor({ username }) {
+  return (
+    <>
+      <a href={`https://github.com/${username}`} target="_blank" class="transform
+        transition duration-100 hover:scale-100 scale-75">
+        <img
+          class="w-20 h-20 m-0 p-0 rounded pointer-events-none"
+          src={"https://github.com/" + username + ".png"}
+        />
+      </a>
+    </>
+  );
+}
+
+export function Contributors({ list }) {
+  return (
+    <div class="flex flex-row flex-wrap gap-4 pt-5">
+      {list.map((username) => {
+        return <Contributor username={username}/>;
+      })}
+    </div>
+  );
+}
