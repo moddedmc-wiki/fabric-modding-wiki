13760d893068a3ac236776808c61777503844454
Calum <contact@mineblock11.dev>
2023-08-05 01:12:13 +0100

    Frontpage rework.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,17 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,md,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,md,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      backgroundSize: {
+        'size-200': '200% 200%',
+      },
+      backgroundPosition: {
+        'pos-0': '0% 0%',
+        'pos-100': '100% 100%',
+      },
+    },
+  },
   plugins: [],
   darkMode: "class",
 };
